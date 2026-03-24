6d5419f6205d4fa36b26c4339217b3f9618a856d
Enzo Bustamante <enzobustamante895@gmail.com>
2026-03-23 22:11:12 -0300

    feat: finish section 7

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,6 +1,6 @@
 import { glob } from "astro/loaders";
 import { z } from "astro/zod";
-import { defineCollection } from "astro:content";
+import { defineCollection, reference } from "astro:content";
 
 const blogCollection = defineCollection({
   loader: glob({ base: "src/content/blog", pattern: "**/*.{md,mdx}" }),
@@ -9,12 +9,30 @@ const blogCollection = defineCollection({
       title: z.string(),
       description: z.string(),
       date: z.coerce.date(),
-      author: z.string(),
+      author: reference("author"),
       image: image(),
       tags: z.array(z.string()),
+
+      //boolean
+      isDraft: z.boolean().default(false),
+    }),
+});
+
+const authorCollection = defineCollection({
+  loader: glob({ base: "src/content/author", pattern: "**/*.yml" }),
+  schema: ({ image }) =>
+    z.object({
+      name: z.string(),
+      avatar: image(),
+      twitter: z.string(),
+      linkedIn: z.string(),
+      github: z.string(),
+      bio: z.string(),
+      subtitle: z.string(),
     }),
 });
 
 export const collections = {
   blog: blogCollection,
+  author: authorCollection,
 };
